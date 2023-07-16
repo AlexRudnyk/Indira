fbdce20b4e9b5ef01c2aa7f2578c4eff23b9f81b
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-07-16 18:31:23 +0300

    adds redux and persist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { RestrictedRoute } from './Routes/RestrictedRoute';
+import { SharedLayout } from './SharedLayout';
+import { HomePage } from 'pages/HomePage';
+import { RegisterPage } from 'pages/RegisterPage';
+import { LoginPage } from 'pages/LoginPage';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomePage />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute component={<RegisterPage />} redirectTo="/" />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute component={<LoginPage />} redirectTo="/" />
+            }
+          />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
---
+++ src/redux/auth/slice.js
@@ -0,0 +1,87 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { signup, login, logout, refreshUser } from './operations';
+
+const initialState = {
+  user: {
+    _id: null,
+    name: null,
+    email: null,
+    phone: null,
+    goodsInCart: [],
+  },
+  accessToken: null,
+  refreshToken: null,
+
+  isLoggedIn: false,
+  isRefreshing: false,
+  error: false,
+};
+
+const handlePending = state => {
+  state.isRefreshing = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isRefreshing = false;
+  state.error = action.payload.message || false;
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  //   reducers: {
+  //     setBalance(state, action) {
+  //       state.user.balance = action.payload;
+  //     },
+  //   },
+  extraReducers: builder => {
+    builder
+      .addCase(signup.pending, handlePending)
+      .addCase(signup.fulfilled, (state, action) => {
+        state.isRefreshing = false;
+        state.error = false;
+      })
+      .addCase(signup.rejected, handleRejected)
+
+      .addCase(login.pending, handlePending)
+      .addCase(login.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.accessToken = action.payload.accessToken;
+
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+        state.error = false;
+      })
+      .addCase(login.rejected, handleRejected)
+
+      .addCase(logout.pending, handlePending)
+      .addCase(logout.fulfilled, (state, action) => {
+        state.user = {
+          _id: null,
+          name: null,
+          email: null,
+          phone: null,
+          goodsInCart: [],
+        };
+        state.accessToken = null;
+        state.refreshToken = null;
+
+        state.isLoggedIn = false;
+        state.isRefreshing = false;
+        state.error = false;
+      })
+      .addCase(logout.rejected, handleRejected)
+
+      .addCase(refreshUser.pending, handlePending)
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+        state.error = false;
+      })
+      .addCase(refreshUser.rejected, handleRejected);
+  },
+});
+
+export const authReducer = authSlice.reducer;
