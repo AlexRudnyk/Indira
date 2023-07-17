cdb9b46ae99e420381429e55fff615a65868bd8d
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-07-17 16:58:04 +0300

    adds form for admin page

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,3 +1,98 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useState } from 'react';
+// import * as yup from 'yup';
+// import { useDispatch } from 'react-redux';
+// import { signup, login } from '../../redux/auth/operations';
+
+// const schema = yup.object().shape({
+//   title: yup
+//     .string()
+//     .min(2)
+//     .max(30, 'must be less than 30 characters')
+//     .required(),
+//   text: yup.string().min(5, 'must be at least 5 characters').required(),
+//   photoURL: yup.string().required(),
+//   price: yup.number().required(),
+// });
+
 export const AdminPage = () => {
-  return <div>AdminPage</div>;
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+  const [photo, setPhoto] = useState('');
+  const [price, setPrice] = useState('');
+
+  const initialValues = {
+    title: '',
+    text: '',
+    photoURL: '',
+    price: '',
+  };
+
+  const formData = new FormData();
+  formData.append('title', title);
+  formData.append('text', text);
+  formData.append('photoURL', photo);
+  formData.append('price', price);
+
+  const handleSubmit = (values, actions) => {
+    console.log('formData', formData);
+    setTitle('');
+    setText('');
+    setPhoto('');
+    setPrice('');
+  };
+
+  return (
+    <>
+      <Formik
+        // validationSchema={schema}
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+      >
+        {({ values, setFieldValue }) => (
+          <Form>
+            <Field
+              value={title}
+              type="text"
+              name="title"
+              placeholder="Enter Titile"
+              onChange={e => setTitle(e.target.value)}
+            />
+            <ErrorMessage name="title" />
+
+            <Field
+              value={text}
+              type="text"
+              name="text"
+              placeholder="Enter Text"
+              onChange={e => setText(e.target.value)}
+            />
+            <ErrorMessage name="text" />
+
+            <input
+              type="file"
+              name="photoURL"
+              onChange={e => {
+                setFieldValue('photoURL', e.target.files[0]);
+                setPhoto(e.target.files[0]);
+              }}
+              accept="image/*,.png,.jpg,.gif,.web"
+            />
+            <ErrorMessage name="photoURL" />
+
+            <Field
+              value={price}
+              type="number"
+              name="price"
+              placeholder="Enter Price"
+              onChange={e => setPrice(e.target.value)}
+            />
+            <ErrorMessage name="price" />
+
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
 };
