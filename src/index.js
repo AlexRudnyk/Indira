88051f1c231bf443f38cc28cec7b6805789ac8f5
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-07-28 21:30:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,8 +10,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <>
+  <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="/indira">
@@ -20,6 +19,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </PersistGate>
     </Provider>
     <ToastContainer />
-  </>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
