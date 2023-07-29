f4330a0e0486068b7e9c2e21d6a0f7bed69a1616
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-07-29 09:50:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from 'redux/store';
+// import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { store, persistor } from 'redux/store';
 // import { App } from 'components/App';
 import './index.css';
-import { ToastContainer } from 'react-toastify';
+// import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="/indira">
-          <p>HELLOOOO!</p>
-          {/* <App /> */}
+          <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
-    <ToastContainer />
+    <ToastContainer /> */}
+    <p>HELLOOOO!</p>
   </React.StrictMode>
 );
